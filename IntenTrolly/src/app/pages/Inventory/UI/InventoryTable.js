14ae0d18dc8f7c282f1959b7572c6361a0545f45
MahimnaUpadhyay <upadhyaymahi28@gmail.com>
2025-09-20 11:17:18 +0530

    UI changes

--- IntenTrolly/src/app/pages/Inventory/UI/InventoryTable.js
+++ IntenTrolly/src/app/pages/Inventory/UI/InventoryTable.js
@@ -111,8 +111,8 @@ const InventoryTable = () => {
 
     return (
         <>
-            <section className="w-full h-auto dark:bg-gray-900 p-3 sm:p-5">
-                <div className="w-full px-4 lg:px-12">
+            <section className="w-full h-auto dark:bg-gray-900 sm:p-5">
+                <div className="w-full bg-gray-200 p-2 rounded-md lg:px-12">
                     <div className="bg-white dark:bg-gray-800 relative shadow-md sm:rounded-lg overflow-hidden">
                         <div className="flex flex-col md:flex-row items-center justify-between space-y-3 md:space-y-0 md:space-x-4 p-4">
                             <div className="w-full md:w-1/2">
